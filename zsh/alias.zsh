9807f3fca19755ab224baddc0b85e8cb4901a12f
Jason Quense <monastic.panic@gmail.com>
2019-04-18 10:49:54 -0400

    setup

---
+++ zsh/alias.zsh
@@ -0,0 +1,10 @@
+
+alias kc=kubectl
+
+alias pir="pip install -r requirements.txt"
+alias pird="pip install -r requirements-dev.txt"
+
+alias n="npm"
+alias yn="yarn"
+
+alias dev-chrome="open -n -a /Applications/Google\ Chrome.app --args --user-data-dir=/tmp/chrome-dev-sess --no-first-run --no-default-browser-check --disable-default-apps"
