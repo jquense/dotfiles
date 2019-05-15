620e555795a946e7c21d459c4be4b03908d47350
Jason Quense <monastic.panic@gmail.com>
2019-05-15 16:27:57 -0400

    clean up

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -8,3 +8,5 @@ alias n="npm"
 alias yn="yarn"
 
 alias dev-chrome="open -n -a /Applications/Google\ Chrome.app --args --user-data-dir=/tmp/chrome-dev-sess --no-first-run --no-default-browser-check --disable-default-apps"
+
+alias reload="source $HOME/.zshrc"
