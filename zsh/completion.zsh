5d826275c97a1e7fbe5df2650dd4124bc1675a1b
Jason Quense <monastic.panic@gmail.com>
2022-06-27 10:48:50 -0400

    Update completion.zsh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,2 +1,2 @@
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
