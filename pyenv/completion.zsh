4e6efdb96086a97ca05e55e106f5826b87ce57db
jquense <monastic.panic@gmail.com>
2025-04-19 10:39:14 -0400

    updates

--- pyenv/completion.zsh
+++ pyenv/completion.zsh
@@ -1,4 +1,11 @@
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init --path)"
-  eval "$(pyenv virtualenv-init -)"
-fi
+# if command -v pyenv 1>/dev/null 2>&1; then
+#   eval "$(pyenv init --path)"
+#   eval "$(pyenv virtualenv-init -)"
+# fi
+
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
